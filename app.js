b0b6913c4f7e95801761068fbc9705a95168564d
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-28 17:44:19 -0500

    add functionality to all 3 dropdowns with the pictures changing accordingly

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const landPicEl = document.getElementById('land-picture');
 // let state
 
 // set event listeners 
+
+waterDropEl.addEventListener('change', () => {
+    const waterPic = waterDropEl.value;
+    waterPicEl.style.backgroundImage = `url(./assets/${waterPic}-water.png)`;
+});
+
+skylineDropEl.addEventListener('change', () => {
+    const skylinePic = skylineDropEl.value;
+    skylinePicEl.style.backgroundImage = `url(./assets/${skylinePic}-skyline.png)`;
+});
+
+landDropEl.addEventListener('change', () => {
+    const landPic = landDropEl.value;
+    landPicEl.style.backgroundImage = `url(./assets/${landPic}-land.png)`;
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
