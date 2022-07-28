0dacfd57abea4967bfa5a79bfa8595f9b382c819
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-28 16:33:21 -0500

    add in basic HTML and CSS for element positioning and DOM

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-// import functions and grab DOM elements
+// DOM elements for the dropdowns
+const waterDropEl = document.getElementById('water-dropdown');
+const skylineDropEl = document.getElementById('skyline-dropdown');
+const landDropEl = document.getElementById('land-dropdown');
+
+// DOM for pictures
+const waterPicEl = document.getElementById('water-picture');
+const skylinePicEl = document.getElementById('skyline-picture');
+const landPicEl = document.getElementById('land-picture');
 
 // let state
 
